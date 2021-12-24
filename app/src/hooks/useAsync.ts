6d6a9f365fae382231825fe947c1beb47178445a
Nick Phura <nickphura@gmail.com>
2021-12-24 15:44:00 -0800

    WIP

--- app/src/hooks/useAsync.ts
+++ app/src/hooks/useAsync.ts
@@ -1,11 +1,11 @@
 import { useRef } from 'react';
 
 export type AsyncFunction<Q extends any[], R> = (...args: Q) => Promise<R>;
-export type AsyncFunctionOnResolved<R, S> = (arg: R) => S | Promise<S>;
-export type AsyncFunctionOnRejected = (error: any) => any;
+export type AsyncFunctionOnResolve<R, S> = (arg: R) => S | Promise<S>;
+export type AsyncFunctionOnReject = (error: any) => any;
 
-export type AsyncFunctionHandler<Q extends any[], S> = (...args: Q) => Promise<void | S>;
-export type AsyncFunctionResetHandler = () => void;
+export type WrappedAsyncFunction<Q extends any[], S> = (...args: Q) => Promise<S>;
+export type ResetWrappedAsyncFunction = () => void;
 
 /**
  * Wraps an async call to prevent duplicate requests if a request is already pending.
@@ -13,34 +13,80 @@ export type AsyncFunctionResetHandler = () => void;
  * Example:
  *
  * ```
- * const [asyncFunctionHandler, resetAsyncFunctionHandler] = useAsync(
- *   (param1: number) => asyncFunction(param1),
+ * const [myAsyncFunction, resetMyAsyncFunction] = useAsync(
+ *   (param: number) => asyncFunction(param1)
+ * )
+ *
+ * await myAsyncFunction(1) // calls `asyncFunction`, where param=1
+ * await myAsyncFunction(2) // returns cached promise from first call, where param=1
+ * resetMyAsyncFunction();  // clears the cached promise
+ * await myAsyncFunction(2) // calls `asyncFunction`, where param=2
+ * ```
+ *
+ * @template Q
+ * @template R
+ * @param {AsyncFunction<Q, R>} asyncFunction the async function to wrap
+ * @return {*}  {[WrappedAsyncFunction<Q, R>, ResetWrappedAsyncFunction]}
+ */
+export const useAsync = <Q extends any[], R>(
+  asyncFunction: AsyncFunction<Q, R>
+): [WrappedAsyncFunction<Q, R>, ResetWrappedAsyncFunction] => {
+  const ref = useRef<Promise<R>>();
+
+  const wrappedAsyncFunction: WrappedAsyncFunction<Q, R> = async (...args) => {
+    if (ref.current) {
+      return ref.current;
+    }
+
+    ref.current = asyncFunction(...args);
+
+    return ref.current;
+  };
+
+  const resetWrappedAsyncFunction = () => {
+    ref.current = undefined;
+  };
+
+  return [wrappedAsyncFunction, resetWrappedAsyncFunction];
+};
+
+/**
+ * Wraps an async call to prevent duplicate requests if a request is already pending.
+ *
+ * Takes `onResolve` and `onReject` callbacks that will be called when the `asyncFunction` resolves or
+ * rejects/catches.
+ *
+ * Example:
+ *
+ * ```
+ * const [myAsyncFunction, resetMyAsyncFunction] = useAsync(
+ *   (param: number) => asyncFunction(param1),
  *   (response: string) => onResolve(response),
  *   (error: any) => onReject(error)
  * )
  *
- * await asyncFunctionHandler(1) // calls the async function, where param=1
- * await asyncFunctionHandler(2) // returns the cached promise from the first call, where param=1
- * resetAsyncFunctionHandler();  // clears the cache
- * await asyncFunctionHandler(2) // calls the async function, where param=2
+ * await myAsyncFunction(1) // calls `asyncFunction`, where param=1
+ * await myAsyncFunction(2) // returns cached promise from first call, where param=1
+ * resetMyAsyncFunction();  // clears the cached promise
+ * await myAsyncFunction(2) // calls `asyncFunction`, where param=2
  * ```
  *
  * @template Q
  * @template R
  * @template S
  * @param {AsyncFunction<Q, R>} asyncFunction the async function to wrap
- * @param {AsyncFunctionOnResolved<R, S>} onResolve called if the promise resolves, receives the response from the asyncFunction
- * @param {AsyncFunctionOnRejected} onReject called if the promise rejects or throws an error, receives an error
- * @return {*}  {[AsyncFunctionHandler<Q, S>, AsyncFunctionResetHandler]}
+ * @param {AsyncFunctionOnResolve<R, S>} onResolve called if the promise resolves, receives the response from the asyncFunction
+ * @param {AsyncFunctionOnReject} onReject called if the promise rejects or catches, receives an error
+ * @return {*}  {[WrappedAsyncFunction<Q, S>, ResetWrappedAsyncFunction]}
  */
-export const useAsync = <Q extends any[], R, S>(
+export const useAsyncHandler = <Q extends any[], R, S>(
   asyncFunction: AsyncFunction<Q, R>,
-  onResolve: AsyncFunctionOnResolved<R, S>,
-  onReject: AsyncFunctionOnRejected
-): [AsyncFunctionHandler<Q, S>, AsyncFunctionResetHandler] => {
+  onResolve: AsyncFunctionOnResolve<R, S>,
+  onReject: AsyncFunctionOnReject
+): [WrappedAsyncFunction<Q, S>, ResetWrappedAsyncFunction] => {
   const ref = useRef<Promise<S>>();
 
-  const asyncFunctionHandler: AsyncFunctionHandler<Q, S> = async (...args) => {
+  const wrappedAsyncFunction: WrappedAsyncFunction<Q, S> = async (...args) => {
     if (ref.current) {
       return ref.current;
     }
@@ -61,9 +107,9 @@ export const useAsync = <Q extends any[], R, S>(
     return ref.current;
   };
 
-  const resetAsyncFunctionHandler = () => {
+  const resetWrappedAsyncFunction = () => {
     ref.current = undefined;
   };
 
-  return [asyncFunctionHandler, resetAsyncFunctionHandler];
+  return [wrappedAsyncFunction, resetWrappedAsyncFunction];
 };
